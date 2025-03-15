bbc7947a193a3121ef2e3165c2035789130844b5
Palak S <psa121@sfu.ca>
2025-03-15 12:59:38 -0700

    Added user input page

--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -1,5 +1,54 @@
+import { useState} from 'react'
+// import './InputPage.css';
+
 function InputPage() {
-  return <h1>Welcome to the Input Page</h1>;
+
+  const [name, setName] = useState("");
+  const [dream, setDream] = useState("");
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    if (name === "username") {
+      setName(value);
+    } else if (name === "dream") {
+      setDream(value);
+    }
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log("Name:", name);
+    console.log("Dream:", dream)
+
+    // pass values to gemini api here
+}
+  
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <header>CloudChaser</header>
+        <h2>Start Building Your Dream</h2>
+          <label>ENTER YOUR NAME:</label>
+          <input 
+            type="text" 
+            name="username" 
+            value={name || ""} 
+            onChange={handleChange}
+          />  
+          <label>ENTER YOUR DREAM:</label>
+          <input 
+            type="text" 
+            name="dream" 
+            value={dream || ""} 
+            onChange={handleChange}
+          />
+
+          <button type="submit">Generate Your Roadmap</button>
+      </form>
+    </>
+  )
 }
 
 export default InputPage;
